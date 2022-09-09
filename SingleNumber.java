7753b0eb57bdd40d2cbec4d813fb0de304d4e27b
26227 <12110718@mail.sustech.edu.cn>
2022-09-09 21:40:52 +0800

    hzr

--- SingleNumber.java
+++ SingleNumber.java
@@ -1,4 +1,16 @@
 package 力扣刷题;
 
 public class SingleNumber {
+    public static void main(String[] args) {
+
+    }
+
+    public int singleNumber(int[] nums) {
+        int ans = nums[0];
+        for (int i = 1; i < nums.length;i++) {
+            ans = ans ^ nums[i];
+        }
+        return ans;
+    }
+
 }
